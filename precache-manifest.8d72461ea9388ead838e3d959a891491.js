a179265014321c93d711b8a23a5d027180e09176
YKalashnikov <uk2709@gmail.com>
2018-10-11 15:35:49 -0700

    Updates

---
+++ precache-manifest.8d72461ea9388ead838e3d959a891491.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "444775e87f38de6d2d02",
+    "url": "/static/js/main.444775e8.chunk.js"
+  },
+  {
+    "revision": "008491fb501354138ec5",
+    "url": "/static/js/1.008491fb.chunk.js"
+  },
+  {
+    "revision": "444775e87f38de6d2d02",
+    "url": "/static/css/main.5a34e6b6.chunk.css"
+  },
+  {
+    "revision": "b30d2186c56dcbd5804532d94ef6809d",
+    "url": "/index.html"
+  }
+];
